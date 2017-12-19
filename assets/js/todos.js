e1ca0c78ff4ffd90c84013a9cbba85ca2b08dccc
Daniele122898 <d.palbanese@gmail.com>
2017-12-19 19:55:52 +0100

    Saves Todo list in local storage

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,20 @@
+//Get local stored list of items
+let todos = [];
+window.onload = function () {
+    todos = localStorage.getItem("todos");
+    console.log(todos);
+    if(todos === null || todos ===""){
+        todos = [];
+    } else {
+        todos = todos.split(",");
+        if (todos.length > 0) {
+            todos.forEach(function (val) {
+                $("ul").append("<li><span><i class='fa fa-trash-o'></i></span> " + val.trim() + "</li>")
+            });
+        }
+    }
+};
+
 //Check off specific todos by clicking
 $("ul").on("click", "li", function () {
     $(this).toggleClass("completed")
@@ -5,10 +22,15 @@ $("ul").on("click", "li", function () {
 
 //remove to-do
 $("ul").on("click", "span", function (event) {
+    let cont = $(this).parent().text().trim();
+    console.log(cont);
     $(this).parent().fadeOut(500, function () {
        $(this).remove();
     });
     event.stopPropagation();
+    //remove from local storage
+    todos.splice(todos.indexOf(cont), 1);
+    localStorage.setItem("todos", todos)
 });
 
 //Create new To-Do
@@ -22,9 +44,14 @@ $("input[type='text']").keypress(function (event) {
         $(this).val("");
         //create a new li and add to ul
         $("ul").append("<li><span><i class='fa fa-trash-o'></i></span> "+todoText+"</li>")
+        //Save to local storage
+        todos.push(todoText.trim());
+        localStorage.setItem("todos", todos)
     }
 });
 
-$(".fa-angle-up").click(function () {
+$("h1").on("click" ,"i", function () {
    $("input[type='text']").fadeToggle();
+    $("h1 i").toggleClass("fa-angle-up");
+    $("h1 i").toggleClass("fa-angle-down");
 });
